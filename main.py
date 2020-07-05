db2bb64a918aee358bfdf10c90db85eed264532a
NusNyar <happypokemonboy@gmail.com>
2020-07-05 15:55:02 +0000

    Added first question

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 print("Hello and welcome to a quiz for the first semester")
-input("Please press space to start")
+space=input("Press space and enter to continue:")
+if space==" ":
+  question_1=input("1. What is the difference between a raster image and a vector image\n a. They're the same type of picture but have different names\n b. A vector image is higher quality and a raster image has lower quality\nc. A raster image is higher quality and a vector image has lower quality")
