630ecdc881fcf0c68948fca6738a530c6597886f
NusNyar <happypokemonboy@gmail.com>
2020-07-11 22:19:37 +0000

    Did question 1,2,3,5 did citation

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
+
+# Made by Ryan and Aprille
+# Took some of the code made by Alicia, Maxwell and Victoria(There was no TM)
+#2020-07-11
+
 print("Hello and welcome to a quiz for the first semester")
 space=input("Press space and enter to continue:")
 if space==" ":
-  question_1=input("1. What is the difference between a raster image and a vector image:\n a. They're the same type of picture but have different names\n b. A vector image is higher quality and a raster image has lower quality\n c. A raster image is higher quality and a vector image has lower quality")
+
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
+question_list = [
+    "1. What is the difference between a raster image and a vector image? \n a) They're the same type of picture but have different names\n b) A vector image is higher quality and a raster image has lower quality(less and more pixels)\n c) A raster image is higher quality and a vector image has lower quality\n\n"
+  
+    "2. Which is the correct code?\n a) print(hello world)\n b) print('hello world)\n c) print('hello world')\n\n"
+
+    "3. What is a set? \n a) An unordered list of items\n b) A list of items that is ordered first in first out\n c) A list of items that is ordered last in first out, first in last out\n\n"
+
+    "4. What is digital forensics? \n a) \n b)\n c)"
+
+    "5. What is a stack? \n a) An unordered list of items\n b) A list of items that is ordered first in first out\n c) A list of items that is ordered last in first out, first in last out\n\n"
+
+    "6. What is camel case? "
+]
+
+question_answers = [
+    Question(question_list[0], "b"),
+    Question(question_list[1], "c"),
+    Question(question_list[2], "a"),
+    Question(question_list[3], "a"),
+    Question(question_list[4], "a"),
+    Question(question_list[5], "a")
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("you got " + str(score) + " out of " + str(len(question_answers)) +
+          " correct")
+
+run_test(questions)
